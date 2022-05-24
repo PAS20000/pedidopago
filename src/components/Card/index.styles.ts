94fe0dd325cf01805b76a1739643550e7d0ca13a
PAS818 <83708869+PS200000@users.noreply.github.com>
2022-05-24 13:42:32 -0300

    Update index.styles.ts

--- src/components/Card/index.styles.ts
+++ src/components/Card/index.styles.ts
@@ -4,6 +4,6 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
    margin: 6.25%;
    padding: 2.25%;
-   border: solid 1px rgba(0,0,0,0.3);
+   box-shadow: 0px 0px 1px 1px rgba(0, 0, 0, 0.1);
    border-radius: 10px;
 `
