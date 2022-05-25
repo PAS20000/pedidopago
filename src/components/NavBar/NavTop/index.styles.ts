1a1388216cb7134d320766107de72ff7ca6bb7f5
PAS818 <83708869+PS200000@users.noreply.github.com>
2022-05-25 13:05:31 -0300

    Update index.styles.ts

--- src/components/NavBar/NavTop/index.styles.ts
+++ src/components/NavBar/NavTop/index.styles.ts
@@ -22,13 +22,13 @@ export const Flex = styled.div`
 `
 
 export const DefaultAtavar = styled.div`
-    background-color: ${props => props.theme.color.primary[1000]};
-    width: 50px;
-    height: 50px;
+    background-color: ${props => props.theme.color.primary[300]};
+    width: 40px;
+    height: 40px;
     border-radius: 50%;
-
+    margin: 5px;
     p{
        text-align: end;
-       padding: 6px;
+       padding: 1px;
     }
 ` 
