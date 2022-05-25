232b236ea1ba81093f7e2f3a8c66f2c128132306
PAS818 <83708869+PS200000@users.noreply.github.com>
2022-05-24 23:56:53 -0300

    Update index.styles.ts

--- src/components/Table/Tbodies/ContributorsBody/index.styles.ts
+++ src/components/Table/Tbodies/ContributorsBody/index.styles.ts
@@ -12,15 +12,17 @@ export const Flex = styled.div`
 export const LabelTd = styled.td`
     text-align: center;
     font-weight: bolder;
-
+   
    .active{
         background-color: ${props => props.theme.color.primary[300]};
         border-radius: 20px;
         padding: 5px;
+        width: 90%;
    }
    .inactive{
        background-color: ${props => props.theme.color.secondary[300]};
        border-radius: 20px;
        padding: 5px;
+       width: 90%;
    }
 `
