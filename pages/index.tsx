f06003759a62ff5e0e5a6f4cab3e6d6433407155
PAS818 <83708869+PS200000@users.noreply.github.com>
2022-05-30 06:09:28 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,7 +98,7 @@ const Home = ({
                 </section>
             </Main>
             <footer>
-                <a href="https://github.com/PS200000/Projects" target='_blank'>Github</a>
+                <a href='https://github.com/PS200000/pedidopago' target='_blank'>Github</a>
             </footer>
         </>
     )
